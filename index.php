d2d0a43b541f14c1a93361a2f52a3a4993366927
MrPaulPhan <paul@mrpaulphan.com>
2017-02-16 03:23:54 -0500

    added gallery field

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
+$images = get_field('gallery');
 
+if( $images ): ?>
+    <ul>
+        <?php foreach( $images as $image ): ?>
+            <li>
+                <a href="<?php echo $image['url']; ?>">
+                     <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+                </a>
+                <p><?php echo $image['caption']; ?></p>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
  ?>

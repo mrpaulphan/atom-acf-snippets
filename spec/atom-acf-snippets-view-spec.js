bc2a88b611555c4c81022ed124127316a10cddc2
MrPaulPhan <paul@mrpaulphan.com>
2017-02-16 03:19:35 -0500

    first
    commit

---
+++ spec/atom-acf-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomAcfSnippetsView from '../lib/atom-acf-snippets-view';
+
+describe('AtomAcfSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
